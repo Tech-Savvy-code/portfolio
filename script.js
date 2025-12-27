50973ff251bdf3b37f4d50dd71d861dcacd5c1c5
OPIYO NICKSON <opiyonick243@gmail.com>
2025-12-27 13:41:22 +0300

    accounts   updated

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+const toggleMoreBtn = document.getElementById('toggle-more');
+const moreIcons = document.querySelector('.more-icons');
 
-  const toggleBtn = document.getElementById('toggle-more');
-  const moreIcons = document.querySelector('.more-icons');
+toggleMoreBtn.addEventListener('click', (e) => {
+  e.preventDefault();
 
-  toggleBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (moreIcons.style.display === 'none') {
-      moreIcons.style.display = 'inline-flex';
-    } else {
-      moreIcons.style.display = 'none';
-    }
-  });
+  // Toggle visibility
+  if (moreIcons.style.display === 'none' || moreIcons.style.display === '') {
+    moreIcons.style.display = 'flex';
+    toggleMoreBtn.setAttribute('data-tooltip', 'Less Accounts');
+  } else {
+    moreIcons.style.display = 'none';
+    toggleMoreBtn.setAttribute('data-tooltip', 'More Accounts');
+  }
+});
