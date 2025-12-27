255b6dc23be4cba098e34d577369725c19983e48
OPIYO NICKSON <opiyonick243@gmail.com>
2025-12-27 12:46:20 +0300

    icons  updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
@@ -5,3 +6,15 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+  const toggleBtn = document.getElementById('toggle-more');
+  const moreIcons = document.querySelector('.more-icons');
+
+  toggleBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (moreIcons.style.display === 'none') {
+      moreIcons.style.display = 'inline-flex';
+    } else {
+      moreIcons.style.display = 'none';
+    }
+  });
